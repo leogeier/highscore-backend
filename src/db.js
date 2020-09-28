ff906229346e8390941cee52ca8473e106967ff7
leogeier <leonard.geier@hotmail.com>
2020-09-29 00:28:52 +0200

    implement db

--- src/db.js
+++ src/db.js
@@ -5,20 +5,63 @@ class DB {
     this.db = new sqlite3.Database(path);
     
     this.db.serialize(() => {
-      this.db.run("CREATE TABLE IF NOT EXISTS scores (entry INTEGER PRIMARY KEY, name TEXT, score INTEGER, time INTEGER)");
+      this.db.run(`
+        CREATE TABLE IF NOT EXISTS scores
+        (id INTEGER PRIMARY KEY,
+         name TEXT,
+         score INTEGER,
+         time INTEGER);
+      `);
+
+      this.insert_row = this.db.prepare(`
+        INSERT INTO scores
+        (name, score, time)
+        VALUES (?, ?, ?);
+      `);
+
+      this.select_highscores = this.db.prepare(`
+        SELECT name, score, time
+        FROM scores
+        ORDER BY score DESC, time, name
+        LIMIT ?;
+      `);
+
+      this.select_row_rank = this.db.prepare(`
+        SELECT ranked.rank
+        FROM
+          (SELECT RANK() OVER(ORDER BY score DESC, time, name) rank, id
+          FROM scores) ranked
+        WHERE ranked.id = ?;
+      `);
     });
   }
 
-  add_score(name, score, time) {
-
+  async add_score(name, score, time) {
+    return await new Promise((resolve, reject) => {
+      this.insert_row.run(name, score, time, function(err) {
+        if (err != null) reject(err);
+        resolve(this.lastID);
+      });
+    });
   }
 
-  get_highscores(count) {
-
+  async get_highscores(count) {
+    return await new Promise((resolve, reject) => {
+      this.select_highscores.all(count, (err, rows) => {
+        if (err != null) reject(err);
+        resolve(rows);
+      });
+    });
   }
 
-  get_scores_around(count_radius) {
-
+  async get_rank(row_id) {
+    return await new Promise((resolve, reject) => {
+      this.select_row_rank.get(row_id, (err, result) => {
+        if (err != null) reject(err);
+        resolve(result.rank);
+      });
+    });
   }
 }
+
  module.exports = DB;
---
+++ test/db.test.js
@@ -0,0 +1,38 @@
+const DB = require("../src/db.js");
+
+let db;
+
+beforeEach(async () => {
+  db = new DB();
+  await db.add_score("angela", 100, 123);
+  await db.add_score("bob", 200, 1234);
+  await db.add_score("chris", 300, 12345);
+  await db.add_score("dana", 5, 1);
+  await db.add_score("eric", 5, 2);
+  await db.add_score("fiona", 5, 2);
+});
+
+test("retrieves high scores", async () => {
+  expect(await db.get_highscores(3)).toEqual([
+    {name: "chris", score: 300, time: 12345},
+    {name: "bob", score: 200, time: 1234},
+    {name: "angela", score: 100, time: 123},
+  ]);
+});
+
+test("does not return more scores than there are in the db", async () => {
+  const highscores = await db.get_highscores(1000);
+  expect(highscores).toHaveLength(6);
+});
+
+test("equals high scores are tie broken by submission time first and name second", async () => {
+  const highscores = await db.get_highscores(6);
+  expect(highscores[3].name).toEqual("dana");
+  expect(highscores[4].name).toEqual("eric");
+  expect(highscores[5].name).toEqual("fiona");
+});
+
+test("gets the current rank of a given row on the leaderboard", async () => {
+  const row_id = await db.add_score("zac", 50, 0);
+  expect(await db.get_rank(row_id)).toEqual(4);
+});
