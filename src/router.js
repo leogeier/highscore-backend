42163b25f60d9b9e44186aa589f37ad977b1dcaa
leogeier <leonard.geier@hotmail.com>
2020-09-29 10:49:29 +0200

    return id after adding score

--- src/router.js
+++ src/router.js
@@ -17,7 +17,7 @@ router.route("/highscore")
     const scores = await db.get_highscores(Math.min(count, config.max_count));
     res.send(scores);
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
     const score = parseInt(req.query.score, 10);
     const time = parseInt(req.query.time, 10);
     const fingerprint = req.query.fingerprint;
@@ -26,15 +26,14 @@ router.route("/highscore")
       return;
     }
     const name = req.query.name.slice(0, config.max_name_length);
-    console.log(name);
 
     if (fingerprint !== get_fingerprint(name, score, time)) {
       res.sendStatus(403);
       return;
     }
 
-    db.add_score(name, score, time);
-    res.sendStatus(201);
+    const id = await db.add_score(name, score, time);
+    res.status(201).send({id});
   });
 
 module.exports = router;
